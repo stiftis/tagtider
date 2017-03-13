5b4701eaa583efad50a165a6635cdd037bc83910
Anton Mattsson <anton.mattsson@learnet.se>
2017-03-13 12:55:44 +0100

    Added ajax

---
+++ ajax/station.php
@@ -0,0 +1,28 @@
+<?php
+$id = $_GET['id'];
+
+$url     = 'http://api.tagtider.net/v1/stations/' . $id . '.json';
+$user    = 'tagtider';
+$pass    = 'codemocracy';
+$options = array(
+    CURLOPT_FRESH_CONNECT   => 1,
+    CURLOPT_URL             => $url,
+    CURLOPT_USERPWD         => $user . ":" . $pass,
+    CURLOPT_HTTPAUTH        => CURLAUTH_DIGEST,
+    CURLOPT_RETURNTRANSFER  => 3,
+);
+
+// Handle and return
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$response = curl_exec($ch);
+
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$header = substr($response, 0, $header_size);
+
+if (!$response) echo curl_error($ch) . " " . curl_errno($ch);
+if($response) echo $response;
+
+curl_close($ch);
+
+?>
