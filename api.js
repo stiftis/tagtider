eee6dd3ff05bb20ab14a9805f452f44dcce231ab
Anton Mattsson <stiftchan@gmail.com>
2017-04-06 23:13:24 +0200

    fix table

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 //fucking kill me
 function stations() {
 
-    $.get("ajax/stations.php")
+    $.getJSON("ajax/stations.php")
         .done(function(data) {
             console.log("done");
             callSuccess(data);
@@ -19,8 +19,8 @@ function stations() {
 
 function callSuccess(data) {
     document.getElementById('result').innerHTML = 'Data retrieved successfully';
-//foreach. json object, adds station.name into select: obj,obj,array,obj
-    for (var station of JSON.parse(data).stations.station) {
+    //foreach. json object, adds station.name into select: obj,obj,array,obj
+    for (var station of data.stations.station) {
         var s = document.getElementById("select_station");
         var option = document.createElement("option");
         option.text = station.name;
@@ -29,48 +29,43 @@ function callSuccess(data) {
 
 }
 
-function getStation(){
+function getStation() {
 
 
-var index = document.getElementById('select_station').selectedIndex;
+    var index = document.getElementById('select_station').selectedIndex;
 
-var id = index;
+    var id = index;
 
-var s='{"id":';
-		s+=id;
-		s+='}';
+    var s = '{"id":';
+    s += id;
+    s += '}';
 
-	 var js_objekt = JSON.parse(s);
+    var js_object = JSON.parse(s);
 
-    $.getJSON("ajax/station.php", js_objekt)
-      .done(function(data) {
-          console.log("done");
-          callSuccessStation(data);
-      })
-      .fail(function() {
-          console.log('no dice: ' + errorCode);
-          document.getElementById('result').innerHTML = 'Error: ' + errorCode;
-      })
-      .always(function() {
-          console.log("complete");
-      });
+    $.getJSON("ajax/station.php", js_object)
+        .done(function(data) {
+            console.log("done");
+            callSuccessStation(data);
+        })
+        .fail(function() {
+            console.log('no dice: ' + errorCode);
+            document.getElementById('result').innerHTML = 'Error: ' + errorCode;
+        })
+        .always(function() {
+            console.log("complete");
+        });
 
 }
 
-function callSuccessStation(data){
+function callSuccessStation(data) {
 
-  if(data.station == null){
-    document.getElementById('data').innerHTML = 'station = null';
-    tableDelete();
-  }else {
-    document.getElementById('data').innerHTML = 'callSuccessStation: ' + data.station.name;
-    tableCreate(data);
-    //obj,obj,obj,array,obj
-    //for (var transfer of data.station.transfers.transfer){
-    //var s = document.getElementById('data2').innerHTML = transfer.arrival;
-    //console.log(s);
-  //}
-}
+    if (data.station == null) {
+        document.getElementById('data').innerHTML = 'station = null';
+        tableDelete();
+    } else {
+        document.getElementById('data').innerHTML = 'callSuccessStation: ' + data.station.name;
+        tableCreate(data);
+    }
 }
 
 function tableCreate(data) {
@@ -78,79 +73,72 @@ function tableCreate(data) {
     tbl.style.width = '100%';
     tbl.id = "tagtabell";
     var tbdy = document.createElement('tbody');
-    //table head list, don't know where if it will work
-    var list = ["Ankomst","Tågnummer", "Origin", "Destination", "Departure"];
+
+    var list = ["Ankomst", "Tågnummer", "Origin", "Destination", "Departure"];
 
     var tr = document.createElement('tr');
 
-    list.forEach( function(item){
+    list.forEach(function(item) {
         var th = document.createElement('th');
         var i = item;
         th.appendChild(document.createTextNode(item));
         tr.appendChild(th);
         console.log(item);
-    tr.appendChild(th);
+        tr.appendChild(th);
     });
 
     tbdy.appendChild(tr);
 
-    //loops foreach
+    //loops foreach in the array transfer with the variable transfer
     for (var transfer of data.station.transfers.transfer) {
-                var tr = document.createElement('tr');
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+
+        //if statement when arrival is none
+        if (transfer.arrival == '0000-00-00 00:00:00') {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode("Placeholder"));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+        } else {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.arrival));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+        }
+            //creates an element of td and attatches the information from transfer.train to the created element. 
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.train));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.origin));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.destination));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+            //newDeparture is always null if there are no changes
+            if (transfer.newDeparture == null) {
+                td = document.createElement('td');
+                td.appendChild(document.createTextNode(transfer.departure));
+                tr.appendChild(td)
+                tbdy.appendChild(tr);
+
+            } else {
                 var td = document.createElement('td');
-
-                //this makes it so i don't have to wonder what 0000-00-00 00:00:00 is
-                if(transfer.arrival == '0000-00-00 00:00:00'){
-                  td.appendChild(document.createTextNode(''));
-                  var td = document.createElement('td');
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-                }else{
-                  var td = document.createElement('td');
-                  td.appendChild(document.createTextNode(transfer.arrival));
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-
-                  td = document.createElement('td');
-                  td.appendChild(document.createTextNode(transfer.train));
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-
-                  td = document.createElement('td');
-                  td.appendChild(document.createTextNode(transfer.origin));
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-
-                  td = document.createElement('td');
-                  td.appendChild(document.createTextNode(transfer.destination));
-
-                  if(transfer.newDeparture == null){
-                  td = document.createElement('td');
-
-                  td.appendChild(document.createTextNode(transfer.departure));
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-
-                } else{
-                  var td = document.createElement('td');
-                  td.appendChild(document.createTextNode(transfer.newDeparture));
-                  tr.appendChild(td)
-                  tbdy.appendChild(tr);
-                }
-                }
-
-                //somehow works when i put tr here
-                //<tr>
-                //<td>transfer.arrival</td>
-                //<td>transfer.train</td>
-                //</tr>
-                console.log(tr);
+                td.appendChild(document.createTextNode(transfer.newDeparture));
+                tr.appendChild(td)
+                tbdy.appendChild(tr);
             }
+        }
     tbl.appendChild(tbdy);
     document.getElementById('data').appendChild(tbl);
-
 }
 
-function tableDelete(){
+function tableDelete() {
     document.getElementById('data') = "";
 }
