0511bb52fc8a377cbcb5495898afd3aef1fd0022
Danny Bui <dannybui@live.se>
2017-04-10 13:24:55 +0200

    SwitchTableArrivals()

--- api.js
+++ api.js
@@ -87,111 +87,8 @@ function getInfo1(data){
     }
 }
 
-function switchTableDeparture() {
-    tableDelete();
-    var index = document.getElementById('select_station').selectedIndex;
-
-    var id = index;
-
-    var s = '{"id":';
-    s += id;
-    s += '}';
-
-    var js_object = JSON.parse(s);
-
-    $.getJSON("ajax/departures.php", js_object)
-        .done(function(data) {
-            console.log("done");
-            tableDeparture(data);
-        })
-        .fail(function() {
-            console.log('no dice: ' + errorCode);
-            document.getElementById('result').innerHTML = 'Error: ' + errorCode;
-        })
-        .always(function() {
-            console.log("complete");
-        });
-
-
-}
-/*
-function tableArrivals(data) {
-    document.getElementById('data') = " ";
-    var tbl = document.createElement('table');
-    tbl.style.width = '100%';
-    tbl.id = "arrivaltabell";
-    var tbdy = document.createElement('tbody');
-
-    var list = ["Ankomst", "Tågnummer", "Origin", "Destination", "Departure"];
-
-    var tr = document.createElement('tr');
-
-    list.forEach(function(item) {
-        var th = document.createElement('th');
-        var i = item;
-        th.appendChild(document.createTextNode(item));
-        tr.appendChild(th);
-        console.log(item);
-        tr.appendChild(th);
-    });
-
-    tbdy.appendChild(tr);
-
-    //loops foreach in the array transfer with the variable transfer
-    for (var transfer of data.station.transfers.transfer) {
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-
-        //if statement when arrival is none
-        if (transfer.arrival == '0000-00-00 00:00:00') {
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode("Placeholder"));
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-        } else {
-            var td = document.createElement('td');
-            td.appendC  hild(document.createTextNode(transfer.arrival));
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-        }
-            //creates an element of td and attatches the information from transfer.train to the created element.
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(transfer.train));
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(transfer.origin));
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(transfer.destination));
-            tr.appendChild(td)
-            tbdy.appendChild(tr);
-            //newDeparture is always null if there are no changes
-            if (transfer.newDeparture == null) {
-                td = document.createElement('td');
-                td.appendChild(document.createTextNode(transfer.departure));
-                tr.appendChild(td)
-                tbdy.appendChild(tr);
-
-            } else {
-                var td = document.createElement('td');
-                td.appendChild(document.createTextNode(transfer.newDeparture));
-                tr.appendChild(td)
-                tbdy.appendChild(tr);
-            }
-        }
-    tbl.appendChild(tbdy);
-    document.getElementById('data').appendChild(tbl);
-}
-*/
-
 //The whole table formatting
 function tableDeparture(data) {
-    if(data.modified == false)
-        return alert("Choose a station!");
     var tbl = document.createElement('table');
     tbl.style.width = '100%';
     tbl.id = "tagtabell";
@@ -270,7 +167,135 @@ function tableDeparture(data) {
     document.getElementById('data').appendChild(tbl);
 }
 
-function tableDelete(){
-     document.getElementById('data').innerHTML = "";
+function switchTableDeparture(){
+    tableDelete();
+    var index = document.getElementById('select_station').selectedIndex;
+
+    var id = index;
+
+    var s = '{"id":';
+    s += id;
+    s += '}';
+
+    var js_object = JSON.parse(s);
+
+    $.getJSON("ajax/departures.php", js_object)
+        .done(function(data) {
+            console.log("done");
+            tableDeparture(data);
+        })
+        .fail(function() {
+            console.log('no dice: ' + errorCode);
+            document.getElementById('result').innerHTML = 'Error: ' + errorCode;
+        })
+        .always(function() {
+            console.log("complete");
+        });
 }
 
+
+function tableArrivals(data) {
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.id = "arrivaltabell";
+    var tbdy = document.createElement('tbody');
+
+    var list = ["Ankomst", "Tågnummer", "Origin", "Destination", "Departure"];
+
+    var tr = document.createElement('tr');
+
+    list.forEach(function(item) {
+        var th = document.createElement('th');
+        var i = item;
+        th.appendChild(document.createTextNode(item));
+        tr.appendChild(th);
+        console.log(item);
+        tr.appendChild(th);
+    });
+
+    tbdy.appendChild(tr);
+
+    //loops foreach in the array transfer with the variable transfer
+    for (var transfer of data.station.transfers.transfer) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+
+        //if statement when arrival is none
+        if (transfer.arrival == '0000-00-00 00:00:00') {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode("Placeholder"));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+        } else {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.arrival));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+        }
+            //creates an element of td and attatches the information from transfer.train to the created element.
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.train));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.origin));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+
+            td = document.createElement('td');
+            td.appendChild(document.createTextNode(transfer.destination));
+            tr.appendChild(td)
+            tbdy.appendChild(tr);
+            //newDeparture is always null if there are no changes
+            if (transfer.newDeparture == null) {
+                td = document.createElement('td');
+                td.appendChild(document.createTextNode(transfer.departure));
+                tr.appendChild(td)
+                tbdy.appendChild(tr);
+
+            } else {
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(transfer.newDeparture));
+                tr.appendChild(td)
+                tbdy.appendChild(tr);
+            }
+        }
+    tbl.appendChild(tbdy);
+    document.getElementById('data').appendChild(tbl);
+}
+
+function switchTableArrival(){
+  tableDelete();
+
+  var index = document.getElementById('select_station').selectedIndex;
+
+  var id = index;
+
+  var s = '{"id":';
+  s += id;
+  s += '}';
+
+  var js_object = JSON.parse(s);
+
+  $.getJSON("ajax/arrivals.php", js_object)
+      .done(function(data) {
+          console.log("done");
+          tableDeparture(data);
+      })
+      .fail(function() {
+          console.log('no dice: ' + errorCode);
+          document.getElementById('result').innerHTML = 'Error: ' + errorCode;
+      })
+      .always(function() {
+          console.log("complete");
+      });
+
+}
+
+
+
+
+function tableDelete() {
+    document.getElementById('data').innerHTML = " ";
+}
