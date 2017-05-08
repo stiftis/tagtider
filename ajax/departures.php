b38c814f1326fe032937d9ad0dc98beb9c05770b
Anton Mattsson <stiftchan@gmail.com>
2017-05-08 14:37:15 +0200

    Comments

--- ajax/departures.php
+++ ajax/departures.php
@@ -5,10 +5,14 @@ $url     = 'http://api.tagtider.net/v1/stations/' . $id . '/transfers/departures
 $user    = 'tagtider';
 $pass    = 'codemocracy';
 $options = array(
+    //Always new connection
     CURLOPT_FRESH_CONNECT   => 1,
     CURLOPT_URL             => $url,
     CURLOPT_USERPWD         => $user . ":" . $pass,
+    //Which authentication is used
     CURLOPT_HTTPAUTH        => CURLAUTH_DIGEST,
+    //return the transfer as a string of the return value of curl_exec()
+    //instead of outputting it out directly.
     CURLOPT_RETURNTRANSFER  => 3,
 );
 
@@ -16,12 +20,7 @@ $options = array(
 $ch = curl_init();
 //set the curl options from the array $options
 curl_setopt_array($ch, $options);
-//
 $response = curl_exec($ch);
-
-
-$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$header = substr($response, 0, $header_size);
 
 if (!$response) echo curl_error($ch) . " " . curl_errno($ch);
 if($response) echo $response;
--- ajax/stations.php
+++ ajax/stations.php
@@ -4,10 +4,14 @@ $url     = 'http://api.tagtider.net/v1/stations.json';
 $user    = 'tagtider';
 $pass    = 'codemocracy';
 $options = array(
+    //Always new connection
     CURLOPT_FRESH_CONNECT   => 1,
     CURLOPT_URL             => $url,
     CURLOPT_USERPWD         => $user . ":" . $pass,
+    //Which authentication is used
     CURLOPT_HTTPAUTH        => CURLAUTH_DIGEST,
+    //return the transfer as a string of the return value of curl_exec()
+    //instead of outputting it out directly.
     CURLOPT_RETURNTRANSFER  => 3,
 );
 
@@ -16,8 +20,8 @@ $ch = curl_init();
 curl_setopt_array($ch, $options);
 $response = curl_exec($ch);
 
-$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$header = substr($response, 0, $header_size);
+//$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+//$header = substr($response, 0, $header_size);
 
 if (!$response) echo curl_error($ch) . " " . curl_errno($ch);
 if($response) echo $response;
